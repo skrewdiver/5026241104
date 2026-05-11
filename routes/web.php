0f0df376e711c2e0a30886f52f1dab730a87d595
skrewdiver <yustisio.priyatno17@gmail.com>
2026-05-11 11:51:02 +0700

    pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController ;
+use App\Http\Controllers\PegawaiDBController ;
 
 Route::get('/', function () {
     return view('welcome');
@@ -46,6 +48,11 @@ Route::get('menu', function () {
 Route::get('linktree', function () {
 	return view('linktree');
 });
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
