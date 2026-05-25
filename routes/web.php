203685c09f2b3feb62ddc46e21d78ab9b8ac9af9
skrewdiver <yustisio.priyatno17@gmail.com>
2026-05-25 11:57:44 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -49,10 +49,18 @@ Route::get('linktree', function () {
 	return view('linktree');
 });
 
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
 Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']);
+Route::get('/pegawai/edit/{id}', [PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update', [PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
